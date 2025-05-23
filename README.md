bd4a8a65649f66510a85f07663a63690168257a5
RuidiH <764342051@qq.com>
2025-05-22 21:15:39 -0700

    adding vec3 for std140 layout and base code for vulkan

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Rhender
+
+```
+g++ -O2 -Wall -std=c++17 main.cpp -o Render.exe -lglfw3 -lvulkan-1 -lpthread
+```
--- include/math/math_utils.hpp
+++ include/math/math_utils.hpp
@@ -1,14 +1,35 @@
 #pragma once
+#include "vec3.hpp"
 
 // TODO: Implement utility functions for each data type 
 
-// min
+// scalar
+template<typename T>
+constexpr T min(T a, T b) noexcept { return a < b ? a : b; }
 
-// max
+template<typename T>
+constexpr T max(T a, T b) noexcept { return a > b ? a : b; }
 
-// clamp
+template<typename T>
+constexpr T clamp(T v, T lo, T hi) noexcept 
+{ return min(max(v, lo), hi); }
 
-// mix ?
+
+// vec3
+template<typename T>
+constexpr Vec3<T> min(const Vec3<T>& a, const Vec3<T>& b) noexcept
+{ return { min(a.x, b.x), min(a.y, b.y), min(a.z, b.z)}; }
+
+template<typename T>
+constexpr Vec3<T> max(const Vec3<T>& a, const Vec3<T>& b) noexcept
+{ return { max(a.x, b.x), max(a.y, b.y), max(a.z, b.z)}; }
+
+template<typename T>
+constexpr Vec3<T> clamp(const Vec3<T>& v, T lo, T hi) noexcept
+{ return { clamp(v.x, lo, hi), 
+           clamp(v.y, lo, hi), 
+           clamp(v.z, lo, hi)}; }
+
 
 // reflect ?
 
